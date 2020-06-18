8e23e390297e307fc6b5f72cfe2ba5298ac9b1e1
Shane Lee <me@shanel.ee>
2020-06-18 09:40:59 +0100

    Update remove node_modules

--- src/public/javascript.js
+++ src/public/javascript.js
@@ -28,7 +28,7 @@ function createMap(suppliers) {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1Ijoic2hhbmVsZWVlIiwiYSI6ImNqd2RzbDQ2bzE1ejQ0OG1vOWNhaWk0a3YifQ.nZMoPave66hDIrmD9OgbzA'
+    accessToken: ''
   }).addTo(mymap)
 
   addCircles(suppliers)
